5d5a663d9976dca532914f30e41aa6274e0ba408
Ayoub Boulbaz <aboulbaz@e2r10p2.1337.ma>
2019-12-19 17:51:28 +0100

    Get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboulbaz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/25 22:48:39 by aboulbaz          #+#    #+#             */
+/*   Updated: 2019/11/27 16:19:28 by aboulbaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char	*ft_strchr(char *str, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *src, unsigned int start, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		get_next_line(int fd, char **line);
+
+#endif
