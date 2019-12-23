3357f41ba3a861c850aa8fbc1c9f58c4f555d96c
Ayoub Boulbaz <aboulbaz@e2r11p3.1337.ma>
2019-12-23 20:51:15 +0100

    fucking normi

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -25,8 +25,7 @@ static int	extractor(char **buf, char **line, char *new_line)
 
 static int	second_case(char **tmp, char **buf, char **line)
 {
-	if(*buf && **buf != '\0')
-		*line = ft_substr(*buf, 0, ft_strlen(*buf));
+	*line = ft_substr(*buf, 0, ft_strlen(*buf));
 	free(*tmp);
 	free(*buf);
 	*buf = 0;
